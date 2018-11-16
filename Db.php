8bd03f70bbc51dbe7091cb12c335df76c9851848
Queue Stainless <qs.900.1.006@gmail.com>
2018-11-16 01:50:56 -0500

    Updated
    Uses static method. Refactored buildQuery method. Added allowed fields.

--- Db.php
+++ Db.php
@@ -1,53 +1,98 @@
 <?php
-/**
- * Inspired by https://phpdelusions.net/pdo/pdo_wrapper
- */
+declare(strict_types=1);
 
-namespace Core;
+namespace Core; // Change to your desired namespace
 
 use \PDO;
 
-class Db extends PDO {
-    private $dsn    = 'mysql:';
-    private $dbhost = 'host=' . DBHOST . ';';
-    private $dbname = 'dbname=' . DBNAME . ';';
-    private $dbuser = DBUSER;
-    private $dbpass = DBPASS;
-    private $chrset = 'charset=utf8';
+// This is not the correct place to put these.
+// You should have a separate config class/file.
+\define('DB_DRIVER', 'mysql');
+\define('DB_HOST', 'mysql');
+\define('DB_NAME', 'mysql');
+\define('DB_CHAR', 'utf8');
+\define('DB_USER', '');
+\define('DB_PASS', '');
+
+/**
+ * Class Model
+ *
+ * Adapted from https://phpdelusions.net/pdo/pdo_wrapper and https://phpdelusions.net/pdo/sql_injection_example
+ *
+ * @package Core
+ */
+class Db {
+    /**
+     * @var \Core\Db
+     */
+    protected static $instance;
+
+    /**
+     * @var \PDO
+     */
+    protected $pdo;
 
     /**
      * Db constructor.
-     * @param array $options
      */
-    public function __construct($options = []) {
+    public function __construct()
+    {
         $default_options = [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
 
-        $options = array_merge($default_options, $options);
+        $dsn = DB_DRIVER . ':'
+               . 'host=' . DB_HOST . ';'
+               . 'dbname=' . DB_NAME . ';'
+               . 'charset=' . DB_CHAR . ';';
 
-        parent::__construct(
-            $this->dsn . $this->dbhost . $this->dbname . $this->chrset,
-            $this->dbuser,
-            $this->dbpass,
-            $options
-        );
+        $this->pdo = new \PDO($dsn, DB_USER, DB_PASS, $default_options);
+    }
+
+    /**
+     * Make the method universally available
+     *
+     * @return \Core\Db
+     */
+    public static function getDB(): Db
+    {
+        if (self::$instance === null) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Proxy to native PDO methods
+     *
+     * @param $method
+     * @param $args
+     *
+     * @return mixed
+     */
+    public function __call(string $method, array $args)
+    {
+        return \call_user_func_array([$this->pdo, $method], $args);
     }
 
     /**
      * Prepares and executes the query statement
-     * @param $query (required) the sql query
-     * @param null $arguments (optional) array of parameters to bind
-     * @return \PDOStatement
+     *
+     * @param string $query     The sql query
+     * @param array  $arguments Array of parameters to bind
+     *
+     * @return bool|\PDOStatement
      */
-    public function run($query, $arguments = null) {
-        if ($arguments == null) :
-            return $this->query($query);
+    public function run(string $query, array $arguments = [])
+    {
+        if ($arguments === null) :
+            return $this->pdo->query($query);
         endif;
 
-        $statement = $this->prepare($query);
+        $statement = $this->pdo->prepare($query);
         $statement->execute($arguments);
 
         return $statement;
@@ -55,84 +100,101 @@ class Db extends PDO {
 
     /**
      * Inserts a new record in the given table
-     * @param string $table
-     * @param array $data
-     * @return int last insert id
+     *
+     * @param string $table   The table into where the data will be inserted
+     * @param array  $data    The data to insert [field => value]
+     * @param array  $allowed Array of allowed fields
+     *
+     * @return int id of the inserted row
      */
-    public function insert($table, $data) {
-        $q = $this->buildQuery(1, $table, $data);
-        $this->run($q['sql'], $q['args']);
-        return $this->lastInsertId();
+    public function insert(string $table, array $data, array $allowed = []): int
+    {
+        $q     = $this->buildQuery($data, $this->getAllowedFields($data, $allowed));
+        $query = 'INSERT INTO ' . $table . $q['sql'];
+        $this->run($query, $q['args']);
+
+        return (int) $this->pdo->lastInsertId();
     }
 
     /**
      * Updates a record in a given table
-     * @param string $table
-     * @param array $data
-     * @param array $where_and
-     * @param array $where_or
-     * @return int
+     *
+     * @param string $table   The table into where the data will be updated
+     * @param array  $data    The data to update [field => value]
+     * @param array  $where   [field => value] limited to '=' comparison operator
+     * @param array  $allowed Array of allowed fields
+     *
+     * @return int Count of affected rows
      */
-    public function update($table, $data, $where_and, $where_or) {
-        $q = $this->buildQuery(2, $table, $data, $where_and, $where_or);
-        $update = $this->run($q['sql'], $q['args']);
+    public function update(string $table, array $data, array $where = [], array $allowed = []): int
+    {
+        $q      = $this->buildQuery($data, $this->getAllowedFields($data, $allowed), $where);
+        $query  = 'UPDATE ' . $table . $q['sql'];
+        $update = $this->run($query, $q['args']);
+
         return $update->rowCount();
     }
 
     /**
      * Builds a query to insert or update data.
-     * @param int $action (required) insert 1 or update 2
-     * @param string $table (required) the name of the table
-     * @param array $data (required) array of columns requested
-     * @param array $where_and (optional) column = value as a string (['{field} {operand}' => ':parameter']). required when using $where_or
-     * @param array $where_or (optional) column = value as a string
-     * @return array of sql and arguments
+     *
+     * @param array $data    [field => value]
+     * @param array $allowed Array of allowed fields
+     * @param array $where   [field => value] limited to '=' comparison operator
+     *
+     * @return array The constructed query and arguments for binding
      */
-    private function buildQuery($action, $table, $data, $where_and = null, $where_or = null) {
-        $fields = [];
-        $args   = [];
-        $sql    = ($action == 1 ? 'INSERT INTO' : 'UPDATE');
-        $where  = '';
+    private function buildQuery(array $data, array $allowed = [], array $where = []): array
+    {
+        $arguments = [];
+        $set_stmt  = '';
+        $set_where = '';
 
-        if ($where_and != null) :
+        if (!empty($where)) {
             $counter = 0;
-            foreach ($where_and as $key => $value) :
-                // $key includes key and operand; let's separate them
-                $split   = explode(' ', $key);
-                $key     = $split[0];
-                $operand = $split[1];
-                $where   .= ($counter == 0 ? 'WHERE' : 'AND') . ' `' . $key . '` ' . $operand .' :' . $key . '_a' . PHP_EOL;
-                $args[':' . $key . '_a'] = $value;
+            foreach ($where as $key => $value) {
+                $set_where .= ($counter ? 'AND' : 'WHERE');
+                $set_where .= ' `' . $key . '` = :' . $key . PHP_EOL;
+
+                $arguments[':' . $key] = $value;
                 $counter++;
-            endforeach;
-            if ($where_or != null) :
-                $counter = 0;
-                foreach ($where_or as $key => $value) :
-                    $split   = explode(' ', $key);
-                    $key     = $split[0];
-                    $operand = $split[1];
-                    $where   .= 'OR `' . $key . '` ' . $operand .' :' . $key . '_o' . PHP_EOL;
-                    $args[':' . $key . '_o'] = $value;
-                    $counter++;
-                endforeach;
-            endif;
-        endif;
+            }
+        }
 
-        foreach ($data as $key => $value) :
-            $key = explode(' ', $key);
-            $fields[] = '`' . $key[0] . '` = :' . $key[0] . PHP_EOL;
-            $args[':' . $key[0]] = $value;
-        endforeach;
-        
-        $table = '`' . str_replace('`', '``', $table) . '`';
+        foreach ($allowed as $key) {
+            if (isset($data[$key])) {
+                $set_stmt .= '`' . str_replace('`', '``', $key) . '` = :' . $key . PHP_EOL . ',';
 
-        $sql .= $table . PHP_EOL . 'SET ' . implode(',', $fields) . $where;
+                $arguments[':' . $key] = $data[$key];
+            }
+        }
 
-        $constructedQuery = [
+        $sql = PHP_EOL . 'SET ' . rtrim($set_stmt, ',') . $set_where;
+
+        $constructed_query = [
             'sql'  => $sql,
-            'args' => $args
+            'args' => $arguments,
         ];
 
-        return $constructedQuery;
+        return $constructed_query;
+    }
+
+    /**
+     * Gets the array of allowed fields.
+     *
+     * @param array $data    The data to insert/update [field => value]
+     * @param array $allowed Array of allowed fields, will default to data fields if empty
+     *
+     * @return array
+     */
+    private function getAllowedFields(array $data, array $allowed): array
+    {
+        if (empty($allowed)) {
+            foreach ($data as $key => $value) {
+                $allowed[] = $key;
+            }
+        }
+
+        return $allowed;
     }
 }
